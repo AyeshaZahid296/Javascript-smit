83b85dfe219cddfea380278397ffd084ec670e2a
AyeshaZahid296 <ayeshazahid296@gmail.com>
2024-10-12 11:23:04 -0700

    assignment 6-11 updated

--- assignment 6-11/app.js
+++ assignment 6-11/app.js
@@ -1,4 +1,4 @@
-// ..........CHAPTER : 06.................
+// ..........CHAPTER : 06 - 09.................
 
 // Question :1
 // var a = 10;
@@ -62,25 +62,229 @@
 // }
     
 //Question :6
-    let subject1 = prompt("Enter the name of first subject:");
-    let subject2 = prompt("Enter the name of second subject:");
-    let subject3 = prompt("Enter the name of third subject:");
+    // let subject1 = prompt("Enter the name of first subject:");
+    // let subject2 = prompt("Enter the name of second subject:");
+    // let subject3 = prompt("Enter the name of third subject:");
     
-    let totalMarks = 100;
+    // let totalMarks = 100;
 
-    let marks1 = parseInt(prompt("Enter obtained marks for " + subject1));
-    let marks2 = parseInt(prompt("Enter obtained marks for " + subject2));
-    let marks3 = parseInt(prompt("Enter obtained marks for " + subject3));
+    // let marks1 = parseInt(prompt("Enter obtained marks for " + subject1));
+    // let marks2 = parseInt(prompt("Enter obtained marks for " + subject2));
+    // let marks3 = parseInt(prompt("Enter obtained marks for " + subject3));
 
-    let totalObtained = marks1 + marks2 + marks3;
-    let percentage = (totalObtained / (totalMarks * 3)) * 100;
+    // let totalObtained = marks1 + marks2 + marks3;
+    // let percentage = (totalObtained / (totalMarks * 3)) * 100;
 
-    document.write("<h3>Result:</h3>");
-    document.write("<table border='1'>");
-    document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>");
-    document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + marks1 + "</td><td>" + (marks1 / totalMarks * 100) + "%</td></tr>");
-    document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + marks2 + "</td><td>" + (marks2 / totalMarks * 100) + "%</td></tr>");
-    document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + marks3 + "</td><td>" + (marks3 / totalMarks * 100) + "%</td></tr>");
-    document.write("<tr><td>Total</td><td>" + (totalMarks * 3) + "</td><td>" + totalObtained + "</td><td>" + percentage.toFixed(2) + "%</td></tr>");
-    document.write("</table>");
+    // document.write("<h3>Result:</h3>");
+    // document.write("<table border='1'>");
+    // document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>");
+    // document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + marks1 + "</td><td>" + (marks1 / totalMarks * 100) + "%</td></tr>");
+    // document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + marks2 + "</td><td>" + (marks2 / totalMarks * 100) + "%</td></tr>");
+    // document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + marks3 + "</td><td>" + (marks3 / totalMarks * 100) + "%</td></tr>");
+    // document.write("<tr><td>Total</td><td>" + (totalMarks * 3) + "</td><td>" + totalObtained + "</td><td>" + percentage.toFixed(2) + "%</td></tr>");
+    // document.write("</table>");
+
+// ..........CHAPTER : 09 - 011.................
+
+// Question :1
+// let city = prompt("Enter your city name:");
+// if (city === "Karachi") {
+//     alert("Welcome to the city of lights");
+// }
+
+// // Question :2
+// let gender = prompt("Enter your gender (male/female):");
+// if (gender === "male") {
+//     alert("Good Morning Sir");
+// } else if (gender === "female") {
+//     alert("Good Morning Ma’am");
+// }
+
+// // Question :3
+// let signalColor = prompt("Enter the color of the traffic signal (Red, Yellow, Green):");
+// if (signalColor.toLowerCase() === "red") {
+//     alert("Must Stop");
+// } else if (signalColor.toLowerCase() === "yellow") {
+//     alert("Ready to move");
+// } else if (signalColor.toLowerCase() === "green") {
+//     alert("Move now");
+// }
+
+// // Question :4
+// let fuel = parseFloat(prompt("Enter remaining fuel in your car (in litres):"));
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// }
+
+// // Question :5
+// // ::::(part a )::::
+// let a = 4;
+// if (++a === 5) {
+//     alert("Given condition for variable a is true");
+// }
+// // Output: Alert will be displayed because ++a will increment a to 5, so the condition becomes true.
+
+// // ::::(part b )::::
+// let b = 82;
+// if (b++ === 83) {
+//     alert("Given condition for variable b is true");
+// }
+// // Output: No alert will be displayed because b++ increments b after the check, so the condition is false.
+
+// // ::::(part c )::::
+// let c = 12;
+// if (c++ === 13) {
+//     alert("Condition 1 is true");
+// }
+// if (c === 13) {
+//     alert("Condition 2 is true");
+// }
+// if (++c < 14) {
+//     alert("Condition 3 is true");
+// }
+// if (c === 14) {
+//     alert("Condition 4 is true");
+// }
+// // Output:
+// // Condition 2 is true (because c becomes 13 after c++).
+// // Condition 4 is true (because ++c makes c equal to 14).
+
+// // ::::(part d )::::
+// let materialCost = 20000;
+// let laborCost = 2000;
+// let totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+// // Output: Alert will be displayed because the total cost equals laborCost + materialCost.
+
+
+// // ::::(part e )::::
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+// // Output: Only "True" alert will be displayed.
+
+
+// // ::::(part f )::::
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");
+// }
+// // Output: Alert will be displayed because in lexicographical order, "car" comes before "cat".
+
+
+// // Question :6
+// // Take input for three subjects
+// var subject1Marks = parseInt(prompt("Enter marks obtained in first subject:"));
+// var subject2Marks = parseInt(prompt("Enter marks obtained in second subject:"));
+// var subject3Marks = parseInt(prompt("Enter marks obtained in third subject:"));
+
+// // Total marks for all subjects
+// var totalMarks = 300;
+// var obtainedMarks = subject1Marks + subject2Marks + subject3Marks;
+
+// // Calculate percentage
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// // Determine grade and remarks based on the percentage
+// var grade, remarks;
+// if (percentage >= 80) {
+//     grade = "A-one";
+//     remarks = "Excellent";
+// } else if (percentage >= 70) {
+//     grade = "A";
+//     remarks = "Good";
+// } else if (percentage >= 60) {
+//     grade = "B";
+//     remarks = "You need to improve";
+// } else {
+//     grade = "Fail";
+//     remarks = "Sorry";
+// }
+
+// // Display the result
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("Total marks: " + totalMarks + "<br>");
+// document.write("Marks obtained: " + obtainedMarks + "<br>");
+// document.write("Percentage: " + percentage.toFixed(2) + "%<br>");
+// document.write("Grade: " + grade + "<br>");
+// document.write("Remarks: " + remarks + "<br>");
+
+// // Question :7
+// // Secret number (between 1 and 10)
+// var secretNumber = 7; // You can change this number
+
+// // Ask the user to guess the number
+// var userGuess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+
+// if (userGuess === secretNumber) {
+//     alert("Bingo! Correct answer");
+// } else if (userGuess + 1 === secretNumber) {
+//     alert("Close enough to the correct answer");
+// } else {
+//     alert("Try again!");
+// }
+
+// // Question :8
+// // Take input number
+// var number = parseInt(prompt("Enter a number:"));
+
+// if (number % 3 === 0) {
+//     alert("The number is divisible by 3");
+// } else {
+//     alert("The number is not divisible by 3");
+// }
+
+// // Question :9
+// // Take input number
+// var number = parseInt(prompt("Enter a number:"));
+
+// if (number % 2 === 0) {
+//     alert("The number is even");
+// } else {
+//     alert("The number is odd");
+// }
+
+// // Question :10
+// // Take temperature input
+// var temperature = parseInt(prompt("Enter the temperature:"));
+
+// if (temperature > 40) {
+//     alert("It is too hot outside.");
+// } else if (temperature > 30) {
+//     alert("The Weather today is Normal.");
+// } else if (temperature > 20) {
+//     alert("Today's Weather is cool.");
+// } else if (temperature > 10) {
+//     alert("OMG! Today’s weather is so Cool.");
+// }
+
+// // Question :11
+// // Take inputs for numbers and operation
+// var num1 = parseFloat(prompt("Enter first number:"));
+// var num2 = parseFloat(prompt("Enter second number:"));
+// var operation = prompt("Enter operation (+, -, *, /, %):");
+
+// var result;
+
+// if (operation === "+") {
+//     result = num1 + num2;
+// } else if (operation === "-") {
+//     result = num1 - num2;
+// } else if (operation === "*") {
+//     result = num1 * num2;
+// } else if (operation === "/") {
+//     result = num1 / num2;
+// } else if (operation === "%") {
+//     result = num1 % num2;
+// } else {
+//     alert("Invalid operation");
+// }
+
+// if (result !== undefined) {
+//     alert("The result is: " + result);
+// }
 
